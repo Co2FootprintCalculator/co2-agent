dcaf6c84512b8a46da3e406b6679397484cfba62
Florian <f.kremser@campus.tu-berlin.de>
2020-04-17 00:28:36 +0200

    Adds log messages and exception handling

--- src/main/java/beans/CarDatabaseBean.java
+++ src/main/java/beans/CarDatabaseBean.java
@@ -10,28 +10,30 @@ import util.CO2FootprintProperties;
 
 import java.io.InputStream;
 import java.sql.SQLException;
-import java.util.InvalidPropertiesFormatException;
 
 public class CarDatabaseBean extends AbstractMethodExposingBean {
 
 	private CO2FootprintProperties properties;
 
 	@Override
-	public void doStart() throws Exception {
-		super.doStart();
-		log.info("CarDatabaseBean - starting");
+	public void doStart() {
 		try {
+			super.doStart();
+			log.info("CarDatabaseBean - starting");
 			this.properties = new CO2FootprintProperties();
-		} catch (InvalidPropertiesFormatException e) {
-			log.error(e.getMessage());
-			e.printStackTrace();
+		} catch (Exception e) {
+			log.error(e.getMessage(),e);
 		}
 	}
 
 	@Override
-	public void doStop() throws Exception {
-		super.doStop();
-		log.info("CarDatabaseBean - stopping");
+	public void doStop() {
+		try {
+			super.doStop();
+			log.info("CarDatabaseBean - stopping");
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
+		}
 	}
 
 	/**
@@ -42,46 +44,45 @@ public class CarDatabaseBean extends AbstractMethodExposingBean {
 	 */
 	public void execute() {
 		log.info("CarDatabaseBean - starting database update");
+
 		RestConsumer restConsumer = new RestConsumer(properties);
 		InputStream databaseInputStream;
 		try {
 			databaseInputStream = restConsumer.downloadDatabase();
-
-			restConsumer.close();
-
 			Driver driver = new Driver(properties);
 			driver.deleteAllRows();
 			driver.uploadDataToDatabase(databaseInputStream);
 			driver.close();
 		} catch (Exception e) {
-			log.error(e.getMessage());
-			e.printStackTrace();
+			log.error(e.getMessage(), e);
 		}
 		log.info("CarDatabaseBean - finished database update");
 	}
 
 	@Expose(name = "ACTION#beans.CarDatabaseBean.getBrands", scope = ActionScope.AGENT)
 	public ObjectNode getBrands() {
-		Driver driver = new Driver(properties);
+		log.info("New method invocation - getBrands(...) called");
 		ObjectNode brandsJson = null;
 		try {
+			Driver driver = new Driver(properties);
 			brandsJson = driver.getBrandsAsJson();
 			driver.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
+		} catch (SQLException | ClassNotFoundException e) {
+			log.error(e.getMessage(), e);
 		}
 		return brandsJson;
 	}
 
 	@Expose(name = "ACTION#beans.CarDatabaseBean.getModels", scope = ActionScope.AGENT)
 	public ObjectNode getModels(String brand) {
-		Driver driver = new Driver(properties);
+		log.info("New method invocation - getModels(...) called");
 		ObjectNode modelsJson = null;
 		try {
+			Driver driver = new Driver(properties);
 			modelsJson = driver.getModelsAsJson(brand);
 			driver.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
+		} catch (SQLException | ClassNotFoundException e) {
+			log.error(e.getMessage(), e);
 		}
 
 		return modelsJson;
@@ -89,13 +90,14 @@ public class CarDatabaseBean extends AbstractMethodExposingBean {
 
 	@Expose(name = "ACTION#beans.CarDatabaseBean.getModelsByFuel", scope = ActionScope.AGENT)
 	public ObjectNode getModelsByFuel(String brand, String fuel) {
-		Driver driver = new Driver(properties);
+		log.info("New method invocation - getModelsByFuel(...) called");
 		ObjectNode modelsJson = null;
 		try {
+			Driver driver = new Driver(properties);
 			modelsJson = driver.getModelsByFuelAsJson(brand, fuel);
 			driver.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
+		} catch (SQLException | ClassNotFoundException e) {
+			log.error(e.getMessage(), e);
 		}
 
 		return modelsJson;
@@ -103,13 +105,14 @@ public class CarDatabaseBean extends AbstractMethodExposingBean {
 
 	@Expose(name = "ACTION#beans.CarDatabaseBean.getFuel", scope = ActionScope.AGENT)
 	public ObjectNode getFuel(String brand, String model) {
-		Driver driver = new Driver(properties);
+		log.info("New method invocation - getFuel(...) called");
 		ObjectNode fuelJson = null;
 		try {
+			Driver driver = new Driver(properties);
 			fuelJson = driver.getFuelAsJson(brand, model);
 			driver.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
+		} catch (SQLException | ClassNotFoundException e) {
+			log.error(e.getMessage(), e);
 		}
 
 		return fuelJson;
@@ -117,13 +120,14 @@ public class CarDatabaseBean extends AbstractMethodExposingBean {
 
 	@Expose(name = "ACTION#beans.CarDatabaseBean.getFuelByBrand", scope = ActionScope.AGENT)
 	public ObjectNode getFuelByBrand(String brand) {
-		Driver driver = new Driver(properties);
+		log.info("New method invocation - getFuelByBrand(...) called");
 		ObjectNode fuelJson = null;
 		try {
+			Driver driver = new Driver(properties);
 			fuelJson = driver.getFuelByBrandAsJson(brand);
 			driver.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
+		} catch (SQLException | ClassNotFoundException e) {
+			log.error(e.getMessage(), e);
 		}
 
 		return fuelJson;
@@ -131,13 +135,14 @@ public class CarDatabaseBean extends AbstractMethodExposingBean {
 
 	@Expose(name = "ACTION#beans.CarDatabaseBean.getCarID", scope = ActionScope.AGENT)
 	public ObjectNode getCarID(String brand, String model, String fuel) {
-		Driver driver = new Driver(properties);
+		log.info("New method invocation - getCarID(...) called");
 		ObjectNode idJson = null;
 		try {
+			Driver driver = new Driver(properties);
 			idJson = driver.getCarIdAsJson(brand, model, fuel);
 			driver.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
+		} catch (SQLException | ClassNotFoundException e) {
+			log.error(e.getMessage(), e);
 		}
 
 		return idJson;
@@ -145,13 +150,14 @@ public class CarDatabaseBean extends AbstractMethodExposingBean {
 
 	@Expose(name = "ACTION#beans.CarDatabaseBean.getCar", scope = ActionScope.AGENT)
 	public Car getCar(String carID) {
-		Driver driver = new Driver(properties);
+		log.info("New method invocation - getCar(...) called");
 		Car car = null;
 		try {
+			Driver driver = new Driver(properties);
 			car = driver.getCar(carID);
 			driver.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
+		} catch (SQLException | ClassNotFoundException e) {
+			log.error(e.getMessage(), e);
 		}
 
 		return car;
